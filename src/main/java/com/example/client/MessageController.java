e2688e478a1e6381739cd2c157fc00ef856ef3e4
davidfilimon <filimon.david97@yahoo.com>
2025-10-15 17:09:26 +0300

    a

--- src/main/java/com/example/client/MessageController.java
+++ src/main/java/com/example/client/MessageController.java
@@ -1,17 +1,23 @@
 package com.example.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class MessageController {
 
-    @Value("${message}")
+    @Value("${service.a.message:Valoare implicita}")
     private String message;
 
-    @GetMapping("/message")
-    public String getMessage() {
-        return this.message;
+    @Value("${service.a.port:0000}")
+    private String port;
+
+    @GetMapping("/config")
+    public String getConfig() {
+        return String.format("Service A - Mesaj: %s | Port: %s",
+                this.message, this.port);
     }
 }
